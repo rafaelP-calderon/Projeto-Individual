9ec5b0551efda009068554508168783b76f9ff0f
rafaelP-calderon <rafael.calderon@sptech.school>
2025-11-05 02:43:08 -0300

    Landing Page estruturada com todas as seções

--- Site/public/script.js
+++ Site/public/script.js
@@ -6,38 +6,48 @@ const us = document.querySelector('.aboutUsEnglish');
 const uk = document.querySelector('.aboutUKEnglish');
 const aus = document.querySelector('.aboutAussieEnglish');
 
+const usFlag = document.querySelector('.usFlag');
+const ukFlag = document.querySelector('.ukFlag');
+const aussieFlag = document.querySelector('.aussieFlag');
+
 usBtn.addEventListener('mouseenter', () => {
     us.style.display = 'flex';
     uk.style.display = 'none';
     aus.style.display = 'none';
+    usFlag.style.display = 'block';
 })
 
 ukBtn.addEventListener('mouseenter', () => {
     us.style.display = 'none';
     uk.style.display = 'flex';
     aus.style.display = 'none';
+    ukFlag.style.display = 'block';
 });
 
 aussieBtn.addEventListener('mouseenter', () => {
     us.style.display = 'none';
     uk.style.display = 'none';
     aus.style.display = 'flex';
+    aussieFlag.style.display = 'block';
 });
 
 usBtn.addEventListener('mouseleave', () => {
     us.style.display = 'none';
     uk.style.display = 'none';
     aus.style.display = 'none';
+    usFlag.style.display = 'none';
 })
 
 ukBtn.addEventListener('mouseleave', () => {
     us.style.display = 'none';
     uk.style.display = 'none';
     aus.style.display = 'none';
+    ukFlag.style.display = 'none';
 });
 
 aussieBtn.addEventListener('mouseleave', () => {
     us.style.display = 'none';
     uk.style.display = 'none';
     aus.style.display = 'none';
+    aussieFlag.style.display = 'none';
 });
