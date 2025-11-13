cab7bfff1997f8878ada618d7ffe341389330482
rafaelP-calderon <rafael.calderon@sptech.school>
2025-11-13 00:22:50 -0300

    Modelagem Lógica V2

--- Banco de Dados/script de exemplo.sql
+++ Banco de Dados/script de exemplo.sql
@@ -1,24 +1,34 @@
 create table Usuario (
 	idUsuario int primary key auto_increment,
     nome varchar(90),
-    email varchar(100),
-    senha varchar(100),
+    email varchar(255),
+    senha varchar(90),
     dtCadastro datetime
 );
 
 create table Formulario (
 	idFormulario int primary key auto_increment,
     idade int,
-    dialeto varchar(45),
+    dialeto enum('UK', 'US', 'AUS'),
     familiaridade varchar(45),
-    frequencia varchar(45),
+    frequencia int,
     fkUsuario int,
     constraint fkUsuarioFormulario foreign key (fkUsuario) references Usuario(idUsuario)
 );
 
+create table Sessao (
+	idSessao int primary key auto_increment,
+    acertosDialetos int,
+    errosDialetos int,
+    mediaAcertos int,
+    pontuacao int,
+    fkUsuario int,
+    constraint fkUsuarioSessao foreign key (fkUsuario) references Usuario(idUsuario)
+);
+
 create table Object (
 	idObject int primary key auto_increment,
-    palavra varchar(45)
+    palavra varchar(100)
 );
 
 create table Idioms (
@@ -27,59 +37,51 @@ create table Idioms (
     significado varchar(100)
 );
 
-create table Sessao (
-	idSessao int primary key auto_increment,
-    acertosDialetos int,
-    errosDialetos int,
-    mediaAcertos decimal(4, 2),
-    dialeto enum('UK', 'US', 'AUS'),
-    pontuacao int,
-    fkUsuario int,
-    constraint fkUsuarioSessao foreign key (fkUsuario) references Usuario(idUsuario)
-);
-
-create table SessaoObject (
-    idSessaoObject int primary key auto_increment,
-    fkSessao int,
+create table sessaoObjeto (
+	fkSessao int,
+		constraint fkSessaoObjeto 
+			foreign key (fkSessao) 
+				references Sessao(idSessao),
     fkObject int,
-    pontuacao int,
-    constraint fkSessaoSessaoObject foreign key (fkSessao) references Sessao(idSessao),
-    constraint fkObjectSessaoObject foreign key (fkObject) references Object(idObject)
+		constraint fkObjectObjeto 
+			foreign key (fkObject) 
+				references Object(idObject),
+    pontuacao int
 );
 
-create table SessaoIdioms (
-    idSessaoIdioms int primary key auto_increment,
-    pontuacao int,
+create table sessaoIdioms (
+	fkSessao int,
+		constraint fkSessaoIdioms 
+			foreign key (fkSessao) 
+				references Sessao(idSessao),
     fkIdioms int,
-    fkSessao int,
-    constraint fkSessaoSessaoIdioms foreign key (fkSessao) references Sessao(idSessao),
-    constraint fkIdiomsSessaoIdioms foreign key (fkIdioms) references Idioms(idIdioms)
+		constraint fkIdiomsIdioms 
+			foreign key (fkIdioms) 
+				references Idioms(idIdioms),
+    pontuacao int
 );
 
--- select u.nome as Nome, f.idade as Idade, f.dialeto as Dialeto
--- 	from Usuario as u join Formulario as f 
---     on fkUsuario = idUsuario;
-    
--- select u.nome as Usuário, s.pontuacao as Pontuação 
--- 	from Usuario as u join Sessao as s
---     on fkUsuario = idUsuario;
-    
--- select u.nome as Usuário, s.dialeto as Dialeto, so.pontuacao as Pontuação
--- 	from Usuario as u join Sessao as s on u.idUsuario = s.fkUsuario
---     join SessaoObject as so on so.fkSessao = s.idSessao;
-    
--- select u.nome as Usuário, s.dialeto as Dialeto, i.expressao as 'Expressão de Maior Dificuldade', si.pontuacao as Pontuação
--- 	from Usuario as u join Sessao as s on u.idUsuario = s.fkUsuario
---     join SessaoIdioms as si on si.fkSessao = s.fkUsuario
---     join Idioms as i on si.fkIdioms = idIdioms;
-    
--- select u.nome as Usuário, sum(so.pontuacao + si.pontuacao) as 'Pontuação Total' 
--- 	from Usuario as u join Sessao as s on u.idUsuario = s.fkUsuario
---     join SessaoObject as so on so.fkSessao = s.idSessao
---     join SessaoIdioms as si on si.fkSessao = s.idSessao
---     group by u.nome;
+select u.nome as Nome, f.dialeto as Dialeto, s.acertosDialetos as Acertos, s.errosDialetos as Erros
+	from Usuario as u join Formulario as f
+	on u.idUsuario = f.fkUsuario
+    join Sessao as s on u.idUsuario = f.fkUsuario;
 
--- select u.nome as Usuário, s.dialeto, s.acertosDialetos, s.errosDialetos, 
--- 	round((s.acertosDialetos / (s.acertosDialetos + s.errosDialetos)) * 100, 2) as Percentual
--- 		from Usuario as u inner join Sessao as s 
--- 		on u.idUsuario = s.fkUsuario;
+select u.nome as Nome from Usuario as u join Sessao as s on u.idUsuario = s.fkUsuario where idSessao >= 3;
+
+select u.nome as Nome, i.expressao as Expressão, s.pontuacao as Pontuação 
+	from Usuario u join Sessao s on u.idUsuario = s.fkUsuario
+    join sessaoIdioms si on s.idSessao = si.fkSessao
+    join Idioms i on si.fkIdioms = i.idIdioms;
+    
+select u.nome as Nome, avg(round(s.pontuacao)) as Pontuação 
+	from Usuario u join Sessao s on u.idUsuario = s.fkUsuario
+    group by nome;
+    
+select u.nome as Nome, f.dialeto as Dialeto, s.pontuacao as Pontuação, 
+	case when f.dialeto = 'UK' and s.pontuacao = 100 then 'O inglês britânico é o mais fácil!'
+    when f.dialeto = 'US' and s.pontuacao = 100 then 'O inglês estadunidense é o mais fácil!'
+    when f.dialeto = 'AUS' and s.pontuacao = 100 then 'O inglês australiano é o mais fácil!' 
+    end as 'Inglês Mais Fácil'
+	from Usuario u join Formulario f on u.idUsuario = f.fkUsuario join Sessao s on u.idUsuario = s.fkUsuario;
+	
+    
