c8be37454295dfe994205546b027bd21cfb958a3
rafaelP-calderon <rafael.calderon@sptech.school>
2025-11-15 16:28:48 -0300

    Arquivos essenciais para integrar com a Web-data-viz

--- Site/public/src/database/script-tabelas.sql
+++ Site/public/src/database/script-tabelas.sql
@@ -1,62 +1,65 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+create database Individual;
+use Individual;
 
-/*
-comandos para mysql server
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+create table Usuario (
+	idUsuario int primary key auto_increment,
+    nome varchar(90),
+    email varchar(255),
+    senha varchar(90),
+    dtCadastro datetime
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+create table Formulario (
+	idFormulario int primary key auto_increment,
+    idade int,
+    dialeto enum('UK', 'US', 'AUS'),
+    familiaridade varchar(45),
+    frequencia int,
+    fkUsuario int,
+    constraint fkUsuarioFormulario foreign key (fkUsuario) references Usuario(idUsuario)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table Sessao (
+	idSessao int primary key auto_increment,
+    acertosDialetos int,
+    errosDialetos int,
+    mediaAcertos int,
+    pontuacao int,
+    fkUsuario int,
+    constraint fkUsuarioSessao foreign key (fkUsuario) references Usuario(idUsuario)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+create table Object (
+	idObject int primary key auto_increment,
+    palavra varchar(100)
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+create table Idioms (
+	idIdioms int primary key auto_increment,
+    expressao varchar(100),
+    significado varchar(100)
 );
 
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+create table sessaoObjeto (
+	fkSessao int,
+		constraint fkSessaoObjeto 
+			foreign key (fkSessao) 
+				references Sessao(idSessao),
+    fkObject int,
+		constraint fkObjectObjeto 
+			foreign key (fkObject) 
+				references Object(idObject),
+    pontuacao int
+);
+
+create table sessaoIdioms (
+	fkSessao int,
+		constraint fkSessaoIdioms 
+			foreign key (fkSessao) 
+				references Sessao(idSessao),
+    fkIdioms int,
+		constraint fkIdiomsIdioms 
+			foreign key (fkIdioms) 
+				references Idioms(idIdioms),
+    pontuacao int
+);
