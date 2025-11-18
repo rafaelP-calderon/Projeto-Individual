5af4ca9f5215169e09a0af9abb878cfc85e2d7fc
rafaelP-calderon <rafael.calderon@sptech.school>
2025-11-17 23:59:49 -0300

    Jogo com lógica adicionada, questionário aprimorado

--- Site/public/js/object.js
+++
@@ -1,8 +0,0 @@
-var displayContainer = document.querySelector(".displayLater");
-var displayHide = document.querySelector(".displayNow");
-var displayNow = document.querySelector(".displayNow");
-var start = document.querySelector(".start");
-
-
-
- 
